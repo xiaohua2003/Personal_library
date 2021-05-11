5e720c87538d4c142d7d3de43f2543b42d89cd40
xiaohua2003 <xcai3@ualberta.ca>
2021-05-10 23:40:28 -0600

    update search form

--- routes/authors.js
+++ routes/authors.js
@@ -2,15 +2,24 @@ const express=require('express')
 const router=express.Router()
 const Author=require('../models/author')
 // All Authors Route
-router.get('/', (req, res)=>{
-    res.render('authors/index')
+router.get('/', async (req, res)=>{
+    let searchOptions={}
+    if (req.query.name != null && req.query.name !==''){
+        searchOptions.name=new RegExp(req.query.name, 'i')
+    }
+    try{
+        const authors=await Author.find({searchOptions})
+        res.render('authors/index', {authors:authors})
+    } catch{
+          res.redirect('/')
+    }  
 })
 
 //New Author Route
 router.get('/new', (req, res)=>{
     res.render('authors/new', {author: new Author()})
 })
-//Create Author Router
+//Create Author Router to send data to authors page
 router.post('/', async (req, res)=>{
     const author =  new Author({
         name: req.body.name
